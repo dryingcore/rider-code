ec0c1183449aa13978eee4999e5740239e8585c2
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 22:29:00 +0000

    Jul 14, 2024, 7:29 PM

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Menu, MenuButton, Text, MenuItem, MenuList, Button, ChevronDownIcon } from "@chakra-ui/react";
 
 export const LanguageSelector = () => {
 	return (
@@ -6,6 +6,18 @@ export const LanguageSelector = () => {
 			<Text mb={2} fontSize={"large"}>
 				Language
 			</Text>
+			<Menu>
+				<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+					Javascript
+				</MenuButton>
+				<MenuList>
+					<MenuItem>Download</MenuItem>
+					<MenuItem>Create a Copy</MenuItem>
+					<MenuItem>Mark as Draft</MenuItem>
+					<MenuItem>Delete</MenuItem>
+					<MenuItem>Attend a Workshop</MenuItem>
+				</MenuList>
+			</Menu>
 		</Box>
 	);
 };
