d72217ac61bd29cabdccce7b9851d19783bd782a
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 22:24:59 +0000

    Jul 14, 2024, 7:24 PM

---
+++ src/components/LanguageSelector.jsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export const LanguageSelector = () => {
+	return (
+		<Box>
+			<Text mb={2} fontSize={"large"}>
+				Language
+			</Text>
+		</Box>
+	);
+};
