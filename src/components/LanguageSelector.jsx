2c6e010bbf3bb1a06b0b8effb85578c68f389e6a
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 22:38:52 +0000

    Jul 14, 2024, 7:38 PM

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,4 +1,4 @@
-import { Box, Menu, MenuButton, Text, MenuItem, MenuList, Button, ChevronDownIcon } from "@chakra-ui/react";
+import { Box, Menu, MenuButton, Text, MenuItem, MenuList, Button } from "@chakra-ui/react";
 import { LANGUAGE_VERSIONS } from "../constants";
 
 const languages = Object.entries(LANGUAGE_VERSIONS);
@@ -6,19 +6,17 @@ const languages = Object.entries(LANGUAGE_VERSIONS);
 export const LanguageSelector = () => {
 	return (
 		<Box>
-			<Text mb={2} fontSize={"large"}>
+			<Text mb={2} fontSize={"lg"}>
 				Language
 			</Text>
 			<Menu>
-				<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-					Javascript
-				</MenuButton>
+				<MenuButton as={Button}>Javascript</MenuButton>
 				<MenuList>
 					{languages.map(([language, version]) => (
 						<MenuItem key={language}>
 							{language}
 							&nbsp;
-							<Text as="span" color="gray.500">
+							<Text as="span" color="gray.500" fontSize="sm">
 								{version}
 							</Text>
 						</MenuItem>
