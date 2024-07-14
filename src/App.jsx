832f5018088dc2b492085956cdcdcab8fe23ed01
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 21:55:36 +0000

    Jul 14, 2024, 6:55 PM

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 function App() {
 	return (
-		<Box minH="100vh" bg="#0f0a19" color ="white">
+		<Box minH="100vh" color="white" px={6} py={8 }>
 			asd
 		</Box>
 	);
