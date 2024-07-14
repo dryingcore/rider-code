af07fcc2f39cdbd165a2256e2d0ac0a65a7eccd2
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 22:15:06 +0000

    Jul 14, 2024, 7:15 PM

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -3,10 +3,10 @@ import { Editor } from "@monaco-editor/react";
 import { useState } from "react";
 
 export const CodeEditor = () => {
-	const [value, setValue] = useState(``);
+	const [value, setValue] = useState("");
 	return (
 		<Box>
-			<Editor height="vh" defaultLanguage="javascript" defaultValue="// some comment" theme="vs-dark" value={value} onChange={value => setValue(value)} />;
+			<Editor height="75vh" defaultLanguage="javascript" defaultValue="// some comment" theme="vs-dark" value={value} onChange={value => setValue(value)} />;
 		</Box>
 	);
 };
