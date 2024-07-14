384799cb3017a30d4c04ec551975e9bf04a62c4c
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 22:13:43 +0000

    Jul 14, 2024, 7:13 PM

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
 
 export const CodeEditor = () => {
+	const [value, setValue] = useState(``);
 	return (
 		<Box>
-			<Editor height="75vh" defaultLanguage="javascript" defaultValue="// some comment" theme="vs-dark" />;
+			<Editor height="vh" defaultLanguage="javascript" defaultValue="// some comment" theme="vs-dark" value={value} />;
 		</Box>
 	);
 };
